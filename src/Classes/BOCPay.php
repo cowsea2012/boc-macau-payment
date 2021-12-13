a3fec6313857667399f965e1e44f4ff885f34fc3
kin lao <cowsea2012@gmail.com>
2021-12-13 20:56:50 +0800

    change User-Agent header to empty

--- src/Classes/BOCPay.php
+++ src/Classes/BOCPay.php
@@ -52,7 +52,8 @@ class BOCPay
 
         $this->client = Http::withHeaders([
             'Content-Type' => 'application/json',
-            'Accept' => 'application/json'
+            'Accept' => 'application/json',
+            'User-Agent' => ''
         ]);
 
     }
